7b416974ef1b2a6f06c27efeaac1b725bf85d17b
Uncensored-Developer <osezele.o@gmail.com>
2020-06-14 11:00:31 +0000

    Add courses to DataManager and add activity to list notes

--- app/src/main/java/com/warchief/notekeeper/DataManager.kt
+++ app/src/main/java/com/warchief/notekeeper/DataManager.kt
@@ -5,8 +5,22 @@ class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
 
+    init {
+        initializeCourses()
+    }
+
     private fun initializeCourses() {
         var course = CourseInfo("android_intents", "Android programming with intents")
+        courses[course.courseId] = course
+
+        course = CourseInfo("android_async", "Android Async programming and services")
+        courses[course.courseId] = course
+
+        course = CourseInfo("java_lang", "Java Fundamentals: The java language")
+        courses[course.courseId] = course
+
+        course = CourseInfo("java_core", "Java Fundamentals: The core platform")
+        courses[course.courseId] = course
     }
 
 }
